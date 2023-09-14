fbcc1511b082fa3d414f13c9a03cbbc993f12627
jamesbright <jamesononiwu@gmail.com>
2023-09-14 12:40:39 +0100

    rotate matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Rotate a matrix 90 degrees clockwise
+"""
+
+
+def rotate_2d_matrix(m):
+    """Rotates in-place"""
+    ln = len(m)
+    temp1, temp2 = 0, 0
+
+    for j in range(0, len(m) // 2 + 1):
+        for i in range(j, ln - 1):
+            #put same position in col from back
+            temp1 = m[i][ln - 1]
+            m[i][ln - 1] = m[j][i]
+            # put temp1 in reverse position in row from bottom
+            temp2 = m[ln - 1][ln - 1 - i + j]
+            m[ln - 1][ln - 1 - i + j] = temp1
+            # put temp2 in same position in col from front
+            temp1 = m[ln - 1 - i + j][j]
+            m[ln - 1 - i + j][j] = temp2
+            # put temp1 in reverse position in row from top
+            m[j][i] = temp1
+        ln -= 1
