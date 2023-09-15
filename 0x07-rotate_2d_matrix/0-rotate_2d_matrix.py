ef8d45809a2eddd3ea8c5bccb811c6580a297d94
jamesbright <jamesononiwu@gmail.com>
2023-09-15 06:43:19 +0100

    fixed pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,20 +5,20 @@
 
 def rotate_2d_matrix(m):
     """Rotates elements in-place"""
-    l = len(m)
+    n = len(m)
     temp1, temp2 = 0, 0
 
     for j in range(0, len(m) // 2 + 1):
-        for i in range(j, l - 1):
+        for i in range(j, n - 1):
             # put same position in col from back
-            temp1 = m[i][l - 1]
-            m[i][l - 1] = m[j][i]
+            temp1 = m[i][n - 1]
+            m[i][n - 1] = m[j][i]
             # put temp1 in reverse position in row from bottom
-            temp2 = m[l - 1][l - 1 - i + j]
-            m[l - 1][l - 1 - i + j] = temp1
+            temp2 = m[n - 1][n - 1 - i + j]
+            m[n - 1][n - 1 - i + j] = temp1
             # put temp2 in same position in col from front
-            temp1 = m[l - 1 - i + j][j]
-            m[l - 1 - i + j][j] = temp2
+            temp1 = m[n - 1 - i + j][j]
+            m[n - 1 - i + j][j] = temp2
             # put temp1 in reverse position in row from top
             m[j][i] = temp1
-        l -= 1
+        n -= 1
